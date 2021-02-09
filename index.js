10dbcd9708d7850fef42a8bce542b9543b5af06b
Rafhael Prates <rafhaprates@hotmail.com>
2021-02-09 16:19:31 -0300

    chore: add bodyparser

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 //Estou comunicando para o Express para usar o EJS como motor para desenhar o HTML
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+//Body Parser
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(bodyParser.json);
+//Rotas
 app.get("/", (req, res) => {
     res.render("index");  
 });
@@ -12,6 +18,10 @@ app.get("/", (req, res) => {
 
 app.get("/ask", (req, res) => {
     res.render("ask");
+});
+
+app.post("/saveask", (req, res) => {
+    res.send("Formulario Recebido!");
 });
 
 
